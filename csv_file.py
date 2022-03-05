deee668696ddb50c11bd051e50739a031aeb2b33
keerthivas <94449540+kittuvasa@users.noreply.github.com>
2022-03-05 12:18:20 +0530

    Update csv_file.py

--- csv_file.py
+++ csv_file.py
@@ -1,9 +1,16 @@
 import csv # in line in csv file is seperate data stream/set, the field are seperated using comma. 
 
+''' read data from a txt file, interpret as csv format  using reader class'''
 file = open("path")
 csv_f = csv.reader(file)
 
 for row in csv_f:
   # unpack the row if needed
+ 
+''' write data to csv file writer class '''
+
+
+
+
   
   
