2e0fe9455ed2e6689ddf9933e2bcf43c7a8aa167
keerthivas <94449540+kittuvasa@users.noreply.github.com>
2022-03-08 22:29:32 +0530

    Create regex.py

---
+++ regex.py
@@ -0,0 +1,7 @@
+''' regular expressions are powerful and flexible feature to extract the desired string pattern or format from a string and much more'''
+import re # re - regular expression
+
+'''grep is the linux command line tool help to implement regex, grep l.st file_loc (. is the wildcard) [re.findall(l.st, text)]
+, grep ^fruit file_loc ( *line begin with fruit), grep cat$ file_loc (*line ends with cat)
+The circumflex [^] and the dollar sign [$] are anchor characters.
+'''
