913abbba5b54756c98cfae9307071e5a481a38bc
keerthivas <94449540+kittuvasa@users.noreply.github.com>
2022-03-09 21:36:59 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -5,3 +5,13 @@ import re # re - regular expression
 , grep ^fruit file_loc ( *line begin with fruit), grep cat$ file_loc (*line ends with cat)
 The circumflex [^] and the dollar sign [$] are anchor characters.
 '''
+print(re.search(r"i.g", "bringing"))
+print(re.search(r"^br", "bringing"))
+print(re.search(r"$ing", "bringing"))
+print(re.search(r"^br", "Bringing", re.IGNORECASE))
+print(re.search(r"[!@a-zA-Z0-9]", "anystring"))
+''' [] chracter class'''
+print(re.search(r"[^a-zA-z]", "This is a complete sentence")) #returns first ' ' circumflex inside[] provides a way to find that aren't match with specifie
+print(re.search(r"cats|dogs", "i like cats and dogs")) # | pipe allow to either or search 
+
+
