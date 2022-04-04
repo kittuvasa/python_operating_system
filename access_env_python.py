2fb2cff198716fdb3b0f29f9cb637f84080631e7
keerthivas <94449540+kittuvasa@users.noreply.github.com>
2022-04-04 21:43:25 +0530

    Update access_env_python.py

--- access_env_python.py
+++ access_env_python.py
@@ -16,6 +16,19 @@ print(sys.argv)
 # run file.py one two three
 # output- ["one", "two", "three"]
 
+#########subprocess
+import subprocess
+print(subprocess.run(["date"]))
+print(subprocess.run(["sleep","2"])) # here the interpreter is unvailabel for 2 second until the child process complete
+x = subprocess.run(["ls","file_not_exist"])# return object contain "returncode" attribute value 2. error
+x.returncode # 2 erro!
+#Obtaining the Output of a System Command
+result = subprocess.run(["host",'8.8.8.8'],capture_output = True)
+print(result.returncode)
+print(result.stdout) #output is in form of byte array. a byte can represent only 256 character but thousands of character out there. so we use several specification techique called
+# encoding (UTF-8)indicates which sequence of bytes represent which characters.
+print(result.stdout.decode())
+
 
 
 
