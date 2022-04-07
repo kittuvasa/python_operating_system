bdcd92e396c16268ab728ed7ff3d051d78d37e95
keerthivas <94449540+kittuvasa@users.noreply.github.com>
2022-04-07 21:27:33 +0530

    Update access_env_python.py

--- access_env_python.py
+++ access_env_python.py
@@ -1,6 +1,8 @@
 # environment variables relate to os. hence we use os module
 # source of input of our program can be user input, env and command line arguments even exit status.
 import os
+#The os module provides a portable way of using operating system dependent functionality with Python.
+
 
 # we can get content of environment variables using environ dict present in os module
 print("PATH:", os.environ.get("PATH","")) # windows cmd echo %variable_name%
@@ -11,6 +13,7 @@ print("PATH:", os.environ.get("PATH","")) # windows cmd echo %variable_name%
 # command line arg, parameters taht are passes to a program when it's started
 # file.py
 import sys
+#The sys module provides information about the Python interpreter's constants, functions, and methods.
 print(sys.argv)
 
 # run file.py one two three
