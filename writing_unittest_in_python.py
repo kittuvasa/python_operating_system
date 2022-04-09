b1361b3c96d38e2e6428d306685c8eef2a3aba8f
keerthivas <94449540+kittuvasa@users.noreply.github.com>
2022-04-09 12:51:04 +0530

    Update writing_unittest_in_python.py

--- writing_unittest_in_python.py
+++ writing_unittest_in_python.py
@@ -1,2 +1,27 @@
 '''unit test are py file with name with isolated function or method name following _test suffix to it.
 '''
+
+# rearrange.py file
+###############
+import re
+import sys
+
+def rearrange_func(name):
+ raw = "([\w .]*), ([\w .]*)"
+ result = re.search(raw, name)
+ return f"{result[2]} {result[1]}"
+########################
+# rearrange_test.py file
+#################
+from rearrange import rearrange_func
+import unittest
+
+class TestRearrange(unittest.TestCase):
+ def test_basic(self):
+  testcase = "brooklyn, chase"
+  expected = "chase brooklyn"
+  self.assertEqual(rearrange_func(testcase), expected)
+
+unittest.main()
+#########################
+
