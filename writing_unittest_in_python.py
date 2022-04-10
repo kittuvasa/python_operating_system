d12744de8a7fd19a435f378a2d862406f1d68eb7
keerthivas <94449540+kittuvasa@users.noreply.github.com>
2022-04-10 14:37:42 +0530

    Update writing_unittest_in_python.py

--- writing_unittest_in_python.py
+++ writing_unittest_in_python.py
@@ -32,5 +32,28 @@ unittest.main()
 # white box/ transparent code test
 '''the unit test code for a program is developed alogon side or after the actual code is developed the test cases are made with the knowledge of how software works'''
 
+# Error and Exceptions
+# RAISE ERROR
+
+def validate_username(name, minlen):
+ assert type(name) == str, "invalid data type of name, str expected"
+ if minlen<1:
+  raise ValueError("minimum length of username atleast 1")
+ if not name.isalnum():
+  return False
+ return True
+
+# unit test for raise and assert error exceptino:
+import unittest
+from validations import valid_username
+
+class TestvalidateUser(unittest.TestCase):
+ def test_invalid_username(self):
+  self.assertRaises(ValueError, valid_username, "name", -1)
+  
+  
+  
+  
+ 
 
 
